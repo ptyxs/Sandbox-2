99b8353ef79329a4fd5392426b56894ac6c087e8
cassian <ptyxs@free.fr>
2019-04-22 23:16:12 +0200

    Creation of project Sandbox 2 as a github first trial.

---
+++ src/BuildAsString.kt
@@ -0,0 +1,32 @@
+object BuildAString {
+    var value = ""
+
+    val truc = buildString {
+        "bold"
+    }
+
+}
+
+fun main() {
+
+    val style: String
+/*
+
+while (n > 0) {
+        value = value.plus(highestInMap(n)!!.value)
+        n = n - highestInMap(n)!!.key
+    }
+*/
+println(BuildAString.truc)
+    var n = 5
+    style = buildString {
+        while (n > 0) {
+            append("Bold")
+            n--
+
+        }
+    }
+//        style = buildString { append("Bold"); append("Bold") }
+    println(style)
+//    println(buildAString.truc)
+}
