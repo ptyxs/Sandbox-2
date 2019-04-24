1e6eb17b8bbbf57a98c6b3359055ba08f27d3fbf
cassian <ptyxs@free.fr>
2019-04-24 15:51:37 +0200

    Creation of project Sandbox 2 as a github first trial.

---
+++ src/Game.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    val playerName = "Estragon"
+    var hasSteed = false
+    var experiencePoints = 0
+
+
+
+    val pubName = "Unicorn's Horn"
+    var publicanOnDuty = "Jim"
+
+    println("pub name $pubName")
+    println("publican name: $publicanOnDuty ")
+    val menu = arrayListOf( "mead", "wine", "LaCroix" )
+
+    println("menu $menu")
+    val mirror = playerName.reversed()
+    println("Estragon in the mirror: $mirror ")
+    publicanOnDuty = "Jill"
+    println("new publican: $publicanOnDuty")
+}
