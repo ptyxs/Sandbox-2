8cb525c22d916088b51f59497c8f4e876f61df09
cassian <ptyxs@free.fr>
2019-04-25 20:45:32 +0200

    Type

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -1,8 +1,12 @@
 const val MAX_EXPERIENCE: Int = 5000
 
-fun tdo() {
-    TODO()
+fun tdo(): String {
+        TODO()
+//    return foo()
+    // TODO() renvoie le type Nothing qui est un sous-type de tout, le return est implicite
+    // et peut être rajouté sans provoquer une erreur de compilation seulement un avertissment unreachable code
 }
+fun foo() = "ooo"
 
 fun main() {
     val playerName = "Estragon"
@@ -11,4 +15,9 @@ fun main() {
     println(experiencePoints)
     println(playerName)
     println(MAX_EXPERIENCE)
+
+//    tdo()
+    val value: Any
+    value = Any()
+print(value)
 }
