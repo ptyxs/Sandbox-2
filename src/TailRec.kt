85fc4e6bc114ec30cd3c17c2393b72c330548ab9
cassian <ptyxs@free.fr>
2019-04-25 20:45:03 +0200

    TailRec

---
+++ src/TailRec.kt
@@ -0,0 +1,11 @@
+//package com.fpinkotlin.recursion.exercise01
+
+
+tailrec fun add(a: Int, b: Int): Int = if (b == 0) a else add(inc(a), dec(b))
+
+fun inc(n: Int) = n + 1
+fun dec(n: Int) = n - 1
+
+fun main() {
+    println(add(5,3))
+}
