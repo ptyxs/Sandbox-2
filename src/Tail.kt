257675d249c8f7bb2941d213be6e10d77232f5d5
cassian <ptyxs@free.fr>
2019-04-29 08:18:59 +0200

    Tail

--- src/Tail.kt
+++ src/Tail.kt
@@ -1,7 +1,10 @@
-fun <T> List<T>.tail(): List<T> = if (this.isEmpty()) throw IllegalArgumentException("tail called on empty list")
-    else this.subList(1, this.count())
-// sublist is a view, safe with unmutable list
+fun <T> List<T>.tail(): List<T> =
+    if (isEmpty()) throw IllegalArgumentException("tail called on empty list")
+    else subList(1, count())
+
+// sublist is a view, safe with non mutable list
+
 fun main() {
-    val list = listOf(1,2,3,4)
+    val list = listOf<Int>(1,2,78)
     println(list.tail())
 }
